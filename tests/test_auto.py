a7fadcfc23cc2355d79095fd34d011cb1328b502
smartwa <smartwacaleb@gmail.com>
2024-04-23 13:44:21 +0300

    patch: Test tgpt-auto

---
+++ tests/test_auto.py
@@ -0,0 +1,12 @@
+import unittest
+import tests.base as base
+from pytgpt.auto import AUTO
+
+
+class TestAuto(base.llmBase):
+    def setUp(self):
+        self.bot = AUTO()
+        self.prompt = base.prompt
+
+if __name__ == "__main__":
+    unittest.main()
