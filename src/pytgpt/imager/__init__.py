50d4c3598495eea11acfbd03bda7369c62e5770e
smartwa <smartwacaleb@gmail.com>
2024-05-01 07:34:55 +0300

    patch: Update imager

--- src/pytgpt/imager/__init__.py
+++ src/pytgpt/imager/__init__.py
@@ -1,6 +1,8 @@
 from .imager import Imager
 from .imager import Prodia
+from .imager import AsyncImager
+from .imager import AsyncProdia
 
 __info__ = "Generate images using craiyon.com"
 
-__all__ = ["Imager", "Prodia"]
+__all__ = ["Imager", "Prodia", "AsyncImager", "AsyncProdia"]
