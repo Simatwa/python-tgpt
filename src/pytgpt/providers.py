b0b536935fc98b27cbff5b5ef180bfe33af8fb45
smartwa <smartwacaleb@gmail.com>
2025-04-30 19:48:43 +0300

    Quitting

--- src/pytgpt/providers.py
+++ src/pytgpt/providers.py
@@ -1,6 +1,5 @@
 from pytgpt.phind import PHIND
 from pytgpt.koboldai import KOBOLDAI
-from pytgpt.blackboxai import BLACKBOXAI
 from pytgpt.ai4chat import AI4CHAT
 from pytgpt.gpt4free import GPT4FREE
 from pytgpt.auto import AUTO
@@ -9,7 +8,6 @@ from pytgpt.auto import AUTO
 tgpt_mapper: dict[str, object] = {
     "phind": PHIND,
     "koboldai": KOBOLDAI,
-    "blackboxai": BLACKBOXAI,
     "ai4chat": AI4CHAT,
     "auto": AUTO,
     "gpt4free": GPT4FREE,
