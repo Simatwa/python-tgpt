9d61053dd2d3265384a3a7c5ca501cdecc57f0c9
smartwa <smartwacaleb@gmail.com>
2025-05-01 22:58:13 +0300

    Quitting

--- src/pytgpt/async_providers.py
+++ src/pytgpt/async_providers.py
@@ -2,7 +2,6 @@ from pytgpt.phind import AsyncPHIND
 from pytgpt.openai import AsyncOPENAI
 from pytgpt.koboldai import AsyncKOBOLDAI
 from pytgpt.groq import AsyncGROQ
-from pytgpt.blackboxai import AsyncBLACKBOXAI
 from pytgpt.novita import AsyncNOVITA
 from pytgpt.ai4chat import AsyncAI4CHAT
 from pytgpt.gpt4free import AsyncGPT4FREE
@@ -10,7 +9,6 @@ from pytgpt.gpt4free import AsyncGPT4FREE
 mapper: dict[str, object] = {
     "phind": AsyncPHIND,
     "koboldai": AsyncKOBOLDAI,
-    "blackboxai": AsyncBLACKBOXAI,
     "gpt4free": AsyncGPT4FREE,
     "groq": AsyncGROQ,
     "openai": AsyncOPENAI,
@@ -20,6 +18,5 @@ mapper: dict[str, object] = {
 tgpt_mapper: dict[str, object] = {
     "phind": AsyncPHIND,
     "koboldai": AsyncKOBOLDAI,
-    "blackboxai": AsyncBLACKBOXAI,
     "ai4chat": AsyncAI4CHAT,
 }
